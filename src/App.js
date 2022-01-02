61716a260be8398db8e3ef73cf04fd1eb8f5e8ba
Akhil Jaisur <akhiljaisur19@gmail.com>
2022-01-02 23:15:59 +0530

    2ndJan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import React, { useState } from 'react';
+import FlashcardList from './Component/FlashcardList';
+import './Style/app.css'
 
 function App() {
+  const [flashcards, setFlashcards] = useState(Sample_flashcards);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <FlashcardList flashcards={flashcards} />
     </div>
   );
 }
+
+const Sample_flashcards = [
+  {
+    id: 1,
+    question:
+      'What is red and has a tail above , 1st aplhabet in your Alphabet books ',
+    Hint: 'Hint is yet to be descide ',
+  },
+  {
+    id: 2,
+    question:
+      'What is so colorful and also famed up mix, Its impossible to solve every face at once .  ',
+    Hint: 'Its always been there in your maths testbook also 9 std textbook has its picture on it ',
+  },
+  {
+    id: 3,
+    question:
+      'Some thing which is missing dial and something which now has screens',
+    Hint: 'Hint is yet to be descide ',
+  },
+];
+console.log(Sample_flashcards)
 
 export default App;
