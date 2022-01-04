9bb21b1c4365671775fcca7c626bba0d4cab4166
Akhil Jaisur <akhiljaisur19@gmail.com>
2022-01-05 00:07:43 +0530

    Try

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import GoogleLogout from 'react-google-login';
 
 export default function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId='489777942407-034dfv5qas6v4tujsg7ob7rdpv2u24qt.apps.googleusercontent.com'
+        buttonText='Login'
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+
+      <GoogleLogin
+        clientId='489777942407-034dfv5qas6v4tujsg7ob7rdpv2u24qt.apps.googleusercontent.com'
+        onSuccess={responseGoogle}
+        isSignedIn={true}
+      />
+      <GoogleLogout
+        clientId='489777942407-034dfv5qas6v4tujsg7ob7rdpv2u24qt.apps.googleusercontent.com'
+        buttonText='Logout'
+        onLogoutSuccess={logout}></GoogleLogout>
+    </div>
+  );
 }
