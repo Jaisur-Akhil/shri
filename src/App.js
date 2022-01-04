3888b34daded213b3d841412ce9efece32ad8ad8
Akhil Jaisur <akhiljaisur19@gmail.com>
2022-01-04 22:41:38 +0530

    Pages  file system

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 /** @format */
-import React, { useState, useEffect } from 'react';
-import FlashcardList from './Component/FlashcardList';
+import React from 'react';
 import './Style/app.css';
-import axios from 'axios';
-
+// import Th from './Pages/Th';
+import Home from './Pages/Home';
 function App() {
-  const [flashcards, setFlashcards] = useState(Sample_flashcards);
-  useEffect(() => {
-    axios.get('http://127.0.0.1:3002/sample').then((res) => {
-      setFlashcards(
-        res.data.map((data, index) => {
-          return {
-            id: `${index}`,
-            question: data.question,
-            hint: data.hint,
-          };
-        })
-      );
-      console.log(res.data + 'this is from console');
-    });
-  }, []);
   return (
     <div className='app'>
-      <FlashcardList flashcards={flashcards} />
+      {/* <Th/> */}
+      <Home />
     </div>
   );
 }
-
-const Sample_flashcards = [
-  {
-    id: 6,
-    question:
-      'What is red and has a tail above , 1st aplhabet in your Alphabet books ',
-    hint: 'Hint is yet to be descide ',
-  },
-  {
-    id: 7,
-    question:
-      'What is so colorful and also famed up mix, Its impossible to solve every face at once .  ',
-    hint: 'Its always been there in your maths testbook also 9 std textbook has its picture on it ',
-  },
-  {
-    id: 8,
-    question:
-      'Some thing which is missing dial and something which now has screens',
-    hint: 'Hint is yet to be descide ',
-  },
-];
-console.log(Sample_flashcards);
-
 export default App;
---
+++ src/Pages/Th.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import React, { useState, useEffect } from 'react';
+import FlashcardList from '../Component/FlashcardList';
+import '../Style/app.css';
+import axios from 'axios';
+export default function TH() {
+  const [flashcards, setFlashcards] = useState(Sample_flashcards);
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3002/sample').then((res) => {
+      setFlashcards(
+        res.data.map((data, index) => {
+          return {
+            id: `${index}`,
+            question: data.question,
+            hint: data.hint,
+          };
+        })
+      );
+      console.log(res.data + 'this is from console');
+    });
+  }, []);
+  return (
+    <div className='app'>
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
+}
+
+const Sample_flashcards = [
+  {
+    id: 6,
+    question:
+      'What is red and has a tail above , 1st aplhabet in your Alphabet books ',
+    hint: 'Hint is yet to be descide ',
+  },
+  {
+    id: 7,
+    question:
+      'What is so colorful and also famed up mix, Its impossible to solve every face at once .  ',
+    hint: 'Its always been there in your maths testbook also 9 std textbook has its picture on it ',
+  },
+  {
+    id: 8,
+    question:
+      'Some thing which is missing dial and something which now has screens',
+    hint: 'Hint is yet to be descide ',
+  },
+];
+console.log(Sample_flashcards);
